f599e32bbedb89a2e2a9bdd716702699e64e267b
ThomasMonnier <thomas.monnier@papernest.com>
2022-11-25 17:07:57 +0100

    [UP] sort suppliers

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ st.set_page_config(layout="wide")
 
 list_languages = ['French', 'Spanish', 'Italian', 'Catalan', 'Other']
 list_verticals = ['Electricity', 'Gas', 'Heat', 'Ambigous', 'Other']
-list_suppliers_fr = list(set(['EDF', 'Eni', 'ENGIE', 'Gaz Europeen', 'Sowee', 'Dyneff', 'Ekwateur', 'Mega Energie', 'Total Energies', 'other']))
-list_suppliers_es = list(set(['Endesa', 'Iberdrola', 'Holaluz', 'Fenie', 'Energia XXI', 'Naturgy', 'Total Energies', 'Alcanzia', 'Aldoro', 'Audax', 'Bonpreu Esclat', 'Catllum', 'DRK Energia', 'EDP', 'Energy GO', 'FC Energia', 'Gana Energia', 'Gesternova', 'Nabalia', 'Podo', 'Repsol', 'Solelec', 'Somenergia', 'Catgas', 'Gas Natural Fenosa', 'Emivasa', 'other']))
-list_suppliers_it = list(set(['Enel', 'Plenitude', 'A2A', 'Repower', 'Iren', 'Acea Energia', 'Sorgenia', 'Gas Sales Energia', 'Wekiwi', 'SEN', 'Edison', 'Hera Comm', 'Axpo', 'Metaenergia', 'Milano Gas e Luce']))
-list_suppliers_all = list(set(list_suppliers_fr + list_suppliers_es + list_suppliers_it))
+list_suppliers_fr = sorted(['EDF', 'Eni', 'ENGIE', 'Gaz Europeen', 'Sowee', 'Dyneff', 'Ekwateur', 'Mega Energie', 'Total Energies', 'other'])
+list_suppliers_es = sorted['Endesa', 'Iberdrola', 'Holaluz', 'Fenie', 'Energia XXI', 'Naturgy', 'Total Energies', 'Alcanzia', 'Aldoro', 'Audax', 'Bonpreu Esclat', 'Catllum', 'DRK Energia', 'EDP', 'Energy GO', 'FC Energia', 'Gana Energia', 'Gesternova', 'Nabalia', 'Podo', 'Repsol', 'Solelec', 'Somenergia', 'Catgas', 'Gas Natural Fenosa', 'Emivasa', 'other'])
+list_suppliers_it = sorted(['Enel', 'Plenitude', 'A2A', 'Repower', 'Iren', 'Acea Energia', 'Sorgenia', 'Gas Sales Energia', 'Wekiwi', 'SEN', 'Edison', 'Hera Comm', 'Axpo', 'Metaenergia', 'Milano Gas e Luce'])
+list_suppliers_all = sorted(list(set(list_suppliers_fr + list_suppliers_es + list_suppliers_it)))
 
 dict_languages = {'French': 'fr', 'Spanish': 'es', 'Italian': 'it', 'Catalan': 'es', 'Other': 'other'}
 dict_verticals = {'Electricity': 'electricity', 'Gas': 'gas', 'Heat': 'heat', 'Ambigous': 'other', 'Other': 'other'}
